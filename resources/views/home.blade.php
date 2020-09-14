cb29626dfb4ea99060ff27dc41f95bce1966f4c1
Christian James Simene <CJSimene>
2020-09-14 23:43:02 +0800

    Starting..

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,17 +7,68 @@
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
+                <div class="card-body text-center">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    You are logged in!
+                    <h3>You are logged in! <u>{{ Auth::user()->name }}</u> </h3>
+
+                    <br><br><hr>
+                    <br>
+
+                    <div>
+                                    <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                        Add Employee
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Fill the Missing Inputs:</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form>  
+                                    <div class="form-group">
+                                        <input type="name" class="form-control" id="fname"  placeholder="Enter First Name">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="name" class="form-control" id="lname"  placeholder="Enter Last Name">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="address" class="form-control" id="address"  placeholder="Enter Your Address">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter E-mail">
+                                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                    </div>
+
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                        
                 </div>
+                <br>
             </div>
-        </div>
+        </div> 
     </div>
+
+
+
 </div>
 @endsection
